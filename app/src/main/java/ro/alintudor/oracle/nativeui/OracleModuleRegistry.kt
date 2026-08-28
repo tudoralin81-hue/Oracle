db13e6519d4336566f002d2125afaf71e2fe8f7f
tudoralin81-hue <tudor.alin81@gmail.com>
2026-08-28 13:41:23 +0200

    Register all seven Oracle modules as native modules

---
+++ app/src/main/java/ro/alintudor/oracle/nativeui/OracleModuleRegistry.kt
@@ -0,0 +1,6 @@
+package ro.alintudor.oracle.nativeui
+
+/** All seven Oracle modules are registered as native application modules. */
+object OracleModuleRegistry {
+    val modules = listOf("PORTFOLIO","ALERTS","NEWS","GROWTH","KNOWLEDGE","ANALYSIS","WATCHLIST")
+}
